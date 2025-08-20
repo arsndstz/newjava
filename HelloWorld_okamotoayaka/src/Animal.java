40b1dcf8053a28d4e792b447de586ce2a4caf76b
arsnn <arsnn@ARSNDSTZ-PC>
2025-08-20 21:06:47 +0900

    課題4_修正

--- HelloWorld_okamotoayaka/src/Animal.java
+++ HelloWorld_okamotoayaka/src/Animal.java
@@ -22,13 +22,7 @@ public class Animal {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		
-		// Q3
-		Animal b = new Animal();
-		System.out.println(b.name);
-		
-		// Q4
-		Animal a = new Animal(3);
-		System.out.println(a.count);
+
 
 	}
 
