ae9dec5108881a65f69789ac1f4735302d0fd94b
arsnn <arsnn@ARSNDSTZ-PC>
2025-09-11 20:06:13 +0900

    課題27修正

--- Animal/src/mainpkg/Main.java
+++ Animal/src/mainpkg/Main.java
@@ -10,35 +10,36 @@ import procpkg.Parakeet;
 import procpkg.Zebra;
 
 public class Main {
-public static void main(String[] args) {
-	
-	System.out.println("コンソールに文字を入力してください");
-	Scanner sc = new Scanner(System.in);
-	String input = sc.nextLine();
-	sc.close();
-	
-	String[] animalData = input.split(",");
-	
-	for (String data : animalData) {
-	String[] parts = data.split(":");
-	String name = parts[0];
-	double length = Double.parseDouble(parts[1]);
-	int speed = Integer.parseInt(parts[2]);
-	
-	Animal animal = null;
-	switch (name) {
-		case "ライオン": animal = new Lion(length, speed); break;
-		case "ゾウ": animal = new Elephant(length, speed); break;
-		case "パンダ": animal = new Panda(length, speed); break;
-		case "チンパンジー": animal = new Chimpanzee(length, speed); break;
-		case "シマウマ": animal = new Zebra(length, speed); break;
-		case "インコ": animal = new Parakeet(length, speed); break;
-		default: System.out.println(name + " は未登録です");
+	public static void main(String[] args) {
+		
+		System.out.println("コンソールに文字を入力してください");
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		
+		String[] animalData = input.split(",");
+		
+		for (String data : animalData) {
+			String[] parts = data.split(":");
+			String name = parts[0];
+			double length = Double.parseDouble(parts[1]);
+			int speed = Integer.parseInt(parts[2]);
+			
+			Animal animal = null;
+			switch (name) {
+				case "ライオン": animal = new Lion(length, speed); break;
+				case "ゾウ": animal = new Elephant(length, speed); break;
+				case "パンダ": animal = new Panda(length, speed); break;
+				case "チンパンジー": animal = new Chimpanzee(length, speed); break;
+				case "シマウマ": animal = new Zebra(length, speed); break;
+				case "インコ": animal = new Parakeet(length, speed); break;
+				default: System.out.println(name + " は未登録です");
+			}
+			
+			if (animal != null) {
+				animal.printInfo();
+			}
+		}
+		
+		sc.close();
 	}
-	
-	if (animal != null) {
-		animal.printInfo();
-	}
-	}
-}
 }
